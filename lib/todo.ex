541bad79df939a5b77471e522bc46481fc9548e0
klambycom <christian@klamby.com>
2016-06-26 23:29:56 +0200

    Add function for updating a entry

--- lib/todo.ex
+++ lib/todo.ex
@@ -76,4 +76,60 @@ defmodule Todo do
     do: entries
         |> Stream.filter(fn({_, entry}) -> entry.date == date end)
         |> Enum.map(fn({_, entry}) -> entry end)
+
+  @doc """
+  Update a entry in the to-do list with a update function, but raises a
+  exception if the id of the entry is changed.
+
+  ## Example
+
+      iex> Todo.new
+      ...> |> Todo.add_entry(%{date: {2016, 06, 26}, title: "Code"})
+      ...> |> Todo.update_entry(1, &Map.put(&1, :date, {2016, 06, 28}))
+      %Todo{
+        auto_id: 2,
+        entries: %{1 => %{id: 1, date: {2016, 06, 28}, title: "Code"}}
+      }
+
+  It does nothing if the entry do not exists.
+
+      iex> Todo.new
+      ...> |> Todo.add_entry(%{date: {2016, 06, 26}, title: "Code"})
+      ...> |> Todo.update_entry(2, &Map.put(&1, :date, {2016, 06, 28}))
+      %Todo{
+        auto_id: 2,
+        entries: %{1 => %{id: 1, date: {2016, 06, 26}, title: "Code"}}
+      }
+  """
+  def update_entry(
+    %__MODULE__{entries: entries} = todo_list,
+    entry_id,
+    updater_fun
+  ) do
+    case entries[entry_id] do
+      nil -> todo_list
+
+      old_entry ->
+        old_entry_id = old_entry.id # Make sure the id hasn't been changed
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(entries, new_entry.id, new_entry)
+        %__MODULE__{todo_list | entries: new_entries}
+    end
+  end
+
+  @doc """
+  Update a entry in the to-do list.
+
+  ## Example
+
+      iex> Todo.new
+      ...> |> Todo.add_entry(%{date: {2016, 06, 26}, title: "Code"})
+      ...> |> Todo.update_entry(%{id: 1, date: {2016, 06, 28}, title: "Kod"})
+      %Todo{
+        auto_id: 2,
+        entries: %{1 => %{id: 1, date: {2016, 06, 28}, title: "Kod"}}
+      }
+  """
+  def update_entry(todo_list, %{} = new_entry),
+    do: update_entry(todo_list, new_entry.id, fn(_) -> new_entry end)
 end
